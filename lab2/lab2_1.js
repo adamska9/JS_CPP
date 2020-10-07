1e6fae35b691809d9067d5be78a808a26946745e
adamska9 <54859102+adamska9@users.noreply.github.com>
2020-10-07 17:42:33 +0300

    changed output

--- lab2/lab2_1.js
+++ lab2/lab2_1.js
@@ -5,7 +5,7 @@ let arr1 = [1,2,3,3,7,[8,[6]]], arr2 = [3,[12,[14,2]],5,6,[8,9]];
 const difference = (arr1, arr2) => {
     //creates an arr out of flattened argument arrays
     const arrN = (arr1.flat(Infinity)).concat(arr2.flat(Infinity));
-    arrOut = [], i = 0, result = {};
+    let arrOut = [], i = 0, result = {};
     //fills the concatenated array with nums of 2 given arrays
     for(let i of arrN){
         if(!result[i])
@@ -19,7 +19,7 @@ const difference = (arr1, arr2) => {
             i++;
         }
     }
-    console.log(arrOut);
+    console.log(`Arrays: ${arr1} | ${arr2}\nDifference: ${arrOut}`);
 }
 
 difference(arr1, arr2);
